94280b1750e0f8d34c99562e5b7b088712934f33
jeffngugi <ngugimwathi254@gmail.com>
2019-11-12 22:23:41 +0300

    Initial setups

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.json({ info: 'Node.js, Express, Postgres API' });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
