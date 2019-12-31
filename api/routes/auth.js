3f1481e82d5e284cab781827a32460c4ef9e958b
jeffngugi <ngugimwathi254@gmail.com>
2019-12-31 20:45:24 +0300

    Employees can sign in

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,9 +2,11 @@ import express from 'express';
 import authController from '../controllers/authController';
 const router = express.Router();
 
-const { loginUser } = authController;
+const { loginUser, currentUser } = authController;
+const passport = require('passport');
 
 router.post('/login', loginUser);
+router.get('/current', passport.authenticate('jwt', { session: false }), currentUser);
 router.get('login', (req, res) => {
     res.json({ message: 'Login user' });
 })
