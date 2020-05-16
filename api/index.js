718a5d28057d34bbb3f30f32b7f0214cc00197c8
jeffngugi <ngugimwathi254@gmail.com>
2020-05-16 17:22:38 +0300

    check if its production

--- api/index.js
+++ api/index.js
@@ -43,16 +43,16 @@ app.use("/api/v1/articles", articles);
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.get("*", (req, res) => {
-    res.status(200).send({
-      message: "Almost there bro",
-    });
-  });
-  // app.use(express.static("../client/build"));
-
   // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  //   res.status(200).send({
+  //     message: "Almost there bro",
+  //   });
   // });
+  app.use(express.static("../client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.use("*", (req, res) =>
