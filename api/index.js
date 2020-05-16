0162e7f8e2a39a25e964405565f35280800a66b7
jeffngugi <ngugimwathi254@gmail.com>
2020-05-16 16:49:56 +0300

    check if its production

--- api/index.js
+++ api/index.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const users = require('./routes/users');
-const passport = require('passport');
-const auth = require('./routes/auth');
-const gif = require('./routes/gif');
-const articles = require('./routes/article');
-const fileupload = require('express-fileupload');
-app.use(fileupload({
-  useTempFiles: true
-}));
-const dotenv = require('dotenv');
+const users = require("./routes/users");
+const passport = require("passport");
+const auth = require("./routes/auth");
+const gif = require("./routes/gif");
+const articles = require("./routes/article");
+const fileupload = require("express-fileupload");
+app.use(
+  fileupload({
+    useTempFiles: true,
+  })
+);
+const dotenv = require("dotenv");
 
-app.use(passport.initialize())
-require('./config/passport')(passport);
+app.use(passport.initialize());
+require("./config/passport")(passport);
 dotenv.config();
-
 
 //set headers to prevent CORS
 app.use((req, res, next) => {
@@ -30,17 +31,29 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.use(passport.initialize())
+app.use(passport.initialize());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api/v1/users', users);
-app.use('/api/v1/auth', auth);
-app.use('/api/v1/gifs', gif);
-app.use('/api/v1/articles', articles);
+app.use("/api/v1/users", users);
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/gifs", gif);
+app.use("/api/v1/articles", articles);
 
-app.use('*', (req, res) => res.status(200).send({
-  message: 'Not found, try to add /api/v1 to access the api'
-}))
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("../client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.use("*", (req, res) =>
+  res.status(200).send({
+    message: "Not found, try to add /api/v1 to access the api",
+  })
+);
 
 module.exports = app;
