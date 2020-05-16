2f21ea2096cad5305852a29d7f0b79a1bdd64a63
jeffngugi <ngugimwathi254@gmail.com>
2020-05-16 15:51:33 +0300

    remover dist folder

--- dist/routes/comment.js
+++
@@ -1,6 +0,0 @@
-// import express from 'express';
-// const router = express.Router();
-// import commentController from '../controllers/commentController';
-// const { comment} = commentController;
-// router.post('')
-"use strict";
