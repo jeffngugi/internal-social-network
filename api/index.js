88c7cbbf6fc206b73c1bd522f1c6c373c76acce0
jeffngugi <ngugimwathi254@gmail.com>
2020-05-17 21:24:37 +0300

    added csp

--- api/index.js
+++ api/index.js
@@ -33,25 +33,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(
-  expressCspHeader({
-    directives: {
-      "default-src": [SELF, INLINE, "*"],
-      "script-src": [
-        SELF,
-        INLINE,
-        "*",
-        "*..bootstrapcdn.com",
-        "*.cloudflare.com",
-        "*.jquery.com",
-      ],
-      "style-src": [SELF, INLINE, "*.fontawesome.com", "*.bootstrapcdn.com"],
-      "img-src": ["*"],
-      "font-src": ["*.fontawesome.com/", "*"],
-    },
-  })
-);
-
 app.use(passport.initialize());
 
 app.use(bodyParser.json());
@@ -63,6 +44,24 @@ app.use("/api/v1/articles", articles);
 
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
+  app.use(
+    expressCspHeader({
+      directives: {
+        "default-src": [SELF, INLINE, "*"],
+        "script-src": [
+          SELF,
+          INLINE,
+          "*",
+          "*..bootstrapcdn.com",
+          "*.cloudflare.com",
+          "*.jquery.com",
+        ],
+        "style-src": [SELF, INLINE, "*.fontawesome.com", "*.bootstrapcdn.com"],
+        "img-src": [SELF, "*"],
+        "font-src": ["*.fontawesome.com/", "*"],
+      },
+    })
+  );
   // Set static folder
   // app.get("*", (req, res) => {
   //   res.status(200).send({
